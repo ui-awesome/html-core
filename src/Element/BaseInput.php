2d1da395df238c2949f6e33aa8e830a8cd5be412
Wilmer Arambula <terabytesoftw@gmail.com>
2026-02-05 11:15:35 -0300

    fix(element): Reorder use statements in `BaseInput` class for better organization.

--- src/Element/BaseInput.php
+++ src/Element/BaseInput.php
@@ -20,7 +20,8 @@ use UIAwesome\Html\Attribute\Global\{
     HasTitle,
     HasTranslate,
 };
-use UIAwesome\Html\Attribute\{HasDisabled, HasForm, HasType};
+use UIAwesome\Html\Attribute\{HasDisabled, HasType};
+use UIAwesome\Html\Attribute\Form\HasForm;
 use UIAwesome\Html\Attribute\Values\Attribute;
 use UIAwesome\Html\Core\Base\BaseTag;
 use UIAwesome\Html\Core\Html;
